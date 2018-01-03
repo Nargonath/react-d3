6d4a3f2747893164c3415b574bf3d0d9b276908d
Nargonath <jonas.pauthier@gmail.com>
2018-01-03 16:59:27 +0100

    Display choropleth USA map with h1b visa data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
+import * as d3 from 'd3';
+import _ from 'lodash';
 
 import { Preloader } from './components/Preloader';
+import { CountyMap } from './components/CountyMap';
+import { loadAllData } from './DataHandling';
 
 class App extends Component {
   state = {
     techSalaries: [],
+    countyNames: [],
+    medianIncomes: [],
   };
+
+  componentWillMount() {
+    loadAllData(data => this.setState(data));
+  }
+
+  countyValue(county, techSalariesMap) {
+    const medianHouseHold = this.state.medianIncomes[county.id];
+    const salaries = techSalariesMap[county.name];
+
+    if (!medianHouseHold || !salaries) {
+      return null;
+    }
+
+    const median = d3.median(salaries, d => d.base_salary);
+
+    return { countyID: county.id, value: median - medianHouseHold.medianIncome };
+  }
 
   render() {
     if (this.state.techSalaries.length < 1) {
       return <Preloader />;
     }
 
-    return <div />;
+    const filteredSalaries = this.state.techSalaries;
+    const filteredSalariesMap = _.groupBy(filteredSalaries, 'countyID');
+    const countyValues = this.state.countyNames
+      .map(county => this.countyValue(county, filteredSalariesMap))
+      .filter(d => !_.isNull(d));
+
+    let zoom = null;
+
+    return (
+      <div className="App container">
+        <svg width="1100" height="500">
+          <CountyMap
+            usTopoJson={this.state.usTopoJson}
+            USstateNames={this.state.USstateNames}
+            values={countyValues}
+            x={0}
+            y={0}
+            width={500}
+            height={500}
+            zoom={zoom}
+          />
+        </svg>
+      </div>
+    );
   }
 }
 
